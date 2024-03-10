d31aae8ee027e3ea4b14b5cb59b1744a4b09e262
Aquila Venancio <aquila_os@thotmail.com>
2024-03-10 08:04:46 -0300

    FEATURE: Corrigindo banco local e mapeamento de entidade base

--- src/main/java/com/aquila/udemyacademi/models/entities/Pedido.java
+++ src/main/java/com/aquila/udemyacademi/models/entities/Pedido.java
@@ -1,19 +1,31 @@
 package com.aquila.udemyacademi.models.entities;
 
+import static jakarta.persistence.TemporalType.TIMESTAMP;
+
 import java.io.Serializable;
-import java.time.LocalDate;
+import java.util.Date;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+@Entity
+@Table(name = "pedido")
 @Data
 @EqualsAndHashCode(callSuper = false, of = {"id", "instante"})
 public class Pedido implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private LocalDate instante;
+
+    @Temporal(TIMESTAMP)
+    private Date instante;
 }
